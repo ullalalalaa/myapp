4176d3c7e9275d9a48f3cb34f5349f08c3fc36e9
ullalalalaa <ulla.kalinka@edu.rtu.lv>
2017-02-09 16:04:15 +0200

    update

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Picogram from './main';
+
+ReactDOM.render(<Picogram />, document.getElementById('picogram'));
---
+++ main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render(){
+    return(
+      <div className="container">
+        <h1 className="title">Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
